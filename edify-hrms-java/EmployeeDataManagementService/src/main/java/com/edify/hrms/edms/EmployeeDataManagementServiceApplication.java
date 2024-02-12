1f2df7d1fb6f11070569a616914474fe57735ebe
desetti praneeth <praneethdesetti@gmail.com>
2024-02-12 18:02:20 +0530

    first commit

---
+++ edify-hrms-java/EmployeeDataManagementService/src/main/java/com/edify/hrms/edms/EmployeeDataManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edify.hrms.edms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDataManagementServiceApplication.class, args);
+	}
+
+}
